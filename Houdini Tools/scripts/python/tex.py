49a64991330514b593fb6bb434cdc9dfee6a9b49
lukevanlukevan <lukevrensburg@gmail.com>
2023-03-20 16:07:52 +0200

    Squashed commit of the following:
    commit daa70b65185f0b1f73c6e8e6dcd6098cf65eb404
    Author: lukevanlukevan <lukevrensburg@gmail.com>
    Date:   Mon Mar 20 15:56:55 2023 +0200
    
        tex scale controller

---
+++ Houdini Tools/scripts/python/tex.py
@@ -0,0 +1,17 @@
+import hou
+
+def createAbs():
+    nodes = hou.selectedNodes()
+    path = nodes[0].path()
+    path_list = path.split("/")
+    del path_list[-1]
+    rebuild = "/".join(path_list)
+    rebuild_item = hou.item(rebuild)
+
+    abs = rebuild_item.createNode("RSMathAbs")
+    baseP = nodes[0].position()
+    abs.setPosition(hou.Vector2(baseP[0]-5, baseP[1]))
+    abs.setName("Scale_Controller")
+    for node in nodes:
+        if node.type().name() == "redshift::TextureSampler":
+            node.setInput(0, abs)
