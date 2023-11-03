9b50238f4c46240e824f3cb8c2d1921245fec389
lukevanlukevan <lukevrensburg@gmail.com>
2023-11-03 10:27:06 +0200

    Added LVUpdater

--- scripts/python/LVUpdater.py
+++ scripts/python/LVUpdater.py
@@ -6,25 +6,15 @@ import shutil
 
 
 def update():
-    install_dir = hou.text.expandString("$LV")
-    temp_dir = hou.text.expandString("$HOUDINI_TEMP_DIR") + "/LV-Tools"
+    path = hou.text.expandString("$LV")
+    os.chdir(path)
+    result = subprocess.run('git fetch --dry-run', shell=True, capture_output=True, text=True)
 
-    new_path = os.path.dirname(install_dir)
-    print(new_path)
-    hou.ui.showInFileBrowser(temp_dir)
+    if result.stdout == "":
+        # print("Up to date")
+        if hou.ui.displayMessage("Update available. Download update?", buttons=("Yes", "No")) == 0:
+            subprocess.run('git fetch')
+            hou.ui.displayMessage("Updated. Please restart Houdini.", buttons=("Okay", "Close"))
 
-    git_path = "https://github.com/lukevanlukevan/LV-Tools.git"
-
-    # print("Downloading")
-    shutil.rmtree(temp_dir, ignore_errors=True)
-    os.mkdir(temp_dir)
-
-    command_str = f"git clone '{git_path}' '{temp_dir}'"
-    print(command_str)
-    subprocess.call(["git", "clone", git_path, temp_dir])
-    for f in os.listdir(temp_dir + "/Houdini Tools/"):
-        print(f)
-        shutil.move(temp_dir + "/Houdini Tools/" + f, install_dir + "/" + f)
-    # shutil.move(temp_dir + "/Houdini Tools", new_path + "1")
-
-    print("Downloaded")
+    else:
+        hou.ui.displayMessage("Up to date.", buttons=("Okay", "Close"))
